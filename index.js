47cc3399701dfea526a1a5466186eae435cf0745
Sean Carlisle <s.m.carlisle@gmail.com>
2023-06-28 19:31:28 -0500

    log list and download works

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const port = process.env.PORT || 3000;
 const ping = require('ping');
 const fs = require('fs');
 
+let logs = logsList();
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+app.get('/:filename', (req, res) => {
+  if (logs.indexOf(req.params.filename) >= 0)
+    res.download(`${__dirname}/logs/${req.params.filename}`);
+  else {
+    console.error(`bad path sent ${req.params.filename}`);
+    res.status(404).send({error: 'no log file here ¯\\_(ツ)_/¯'});
+  }
 });
 
 
@@ -22,15 +31,34 @@ function writeToFile(host) {
   console.log(`outputting to file for host:${host}`);
   const timestamps = new Map([...pingCache.get(host)]
     .sort((a, b) => a[0].getTime() - b[0].getTime()));
+  //TODO check for day change to create new log file
+  const days = timestampDays(timestamps);
+
+  if (days.size === 1) {
+    //TODO refactor below
+  }
   let output = '';
   timestamps.forEach((value, key) =>
     output = output.concat(`\n${key.toISOString()} : ${value}`));
+  //TODO GET DATE FROM FIRST ELEMENT/MAP
   const date = new Date().toISOString().slice(0, 10);
   const filename = `logs/${host}-${date}.log`;
   fs.appendFile(filename, output, err => {
     if (err) console.log("writing error")
   });
   pingCache.get(host).clear();
+  //TODO EMIT FILES LIST ON IO.EMIT
+}
+
+function timestampDays(timestamps) {
+  return [...new Set(timestamps.keys().map(value => value.toISOString().slice(0, 10)))];
+}
+
+
+function logsList() {
+  const files = fs.readdirSync(`${__dirname}/logs/`);
+  console.log(files);
+  return files;
 }
 
 function startPolling() {
@@ -60,13 +88,15 @@ function stopPolling() {
     console.log('stop polling: ' + key);
     clearInterval(value);
   });
+  hosts.forEach(host => writeToFile(host));
 }
 
 if (usersCount) startPolling();
 io.on('connection', (socket) => {
   usersCount++;
   console.log('user connected');
-  io.emit('hosts', hosts);
+  socket.emit('hosts', hosts);
+  socket.emit('files', logs);
   if (usersCount === 1) startPolling();
   socket.on('disconnect', () => {
     console.log('user disconnected');
@@ -77,4 +107,8 @@ io.on('connection', (socket) => {
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+process.on('exit', function () {
+  console.log('About to exit.');
+  hosts.forEach(host => writeToFile(host));
 });
